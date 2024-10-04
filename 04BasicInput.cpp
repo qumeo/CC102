16125ef2b61955bcdbf89b6f3d9bd1acebd36416
qumeo <romanoqueen1@gmail.com>
2024-10-04 10:00:01 +0800

    activity

---
+++ 04BasicInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nickname , course_year_level , school;
+	cout << "Nickname: ";
+	cin >> nickname;
+	cout << "Course-Year Level: ";
+	cin >> course_year_level;
+	cout << "School: ";
+	cin >> school;
+	return 0;
+}
