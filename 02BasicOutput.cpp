f3cda31a2e55f4a00a6fe4f3f63b38df4450282d
qumeo <romanoqueen1@gmail.com>
2024-10-04 09:46:02 +0800

    activiy

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSCS-1" << endl;
+	cout << "Queen Loren Lagdameo Romano" << endl;
+	cout << "Purok 1 Laniton Basud Camarines Norte" << endl;
+	return 0;
+}
