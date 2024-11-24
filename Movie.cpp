d7015c29d1913c5d309bb6387896bb2ec4f1143b
Queen Romano <romanoqueen1@gmail.com>
2024-11-24 11:28:46 +0800

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -7,7 +7,7 @@ int main()
 	string parent;
 	cout << "Enter age: "
 	cin >> age;
-	cout (age <= 13 (cout >> "With parents?", cin >> parent, parent == "yes" ? "G,PG": "G") age > 13 && < 16 (cout >> "With parents?", cin >> parent, parent == "yes" ? "G, PG, R" : "G,PG") : age > 16 ? "G, PG, R" : 0);
+	cout (age <= 13 (cout >> "With parents?", cin >> parent, parent == "yes" ? "G,PG": "G") age >= 13 && < 16 (cout >> "With parents?", cin >> parent, parent == "yes" ? "G, PG, R" : "G,PG") : age >= 16 ? "G, PG, R" : 0);
 	float money;
 	cout << (money >= 7.50 ? "Matinee" : money >= 10.50 ? "Evening" : 0);
 	return 0;
